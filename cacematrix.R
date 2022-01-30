4a8f7616ce8cfe67b71f2ca7d85bb5ab70a2eebd
Abhi01697 <98537712+Abhi01697@users.noreply.github.com>
2022-01-30 19:07:49 +0530

    Update cacematrix.R

--- cacematrix.R
+++ cacematrix.R
@@ -1,25 +1,40 @@
-makeVector <- function(x = numeric()) {
-       m <- NULL
-       set <- function(y) {
-              x <<- y
-              m <<- NULL
-       }
-       get <- function() x
-       setmean <- function(mean) m <<- mean
-       getmean <- function() m
-       list(set = set, get = get,
-            setmean = setmean,
-            getmean = getmean)
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+##makeCachematrix consists of set,get,setinv, getinv
+##library(mass) is used to calculate inverse for non square as well as square matrices
+library(mass)
+makeCacheMatrix <- function(x = matrix()) {
+  inv<-NULL            #initializing inverse as NULL
+  set<-function(y){
+                  x<<-y
+                  inv<<-NULL
+                   }
+  get<-function()x              #function to get matrix x
+  setinv<-function(inverse)inv<<-inverse
+  getinv<-function(){
+                  inver<-ginv(x)
+                  inver%*%x           #function to obtain inverse of the matrix
+                  }
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-cachemean <- function(x, ...) {
-       m <- x$getmean()
-       if(!is.null(m)) {
-              message("getting cached data")
-              return(m)
-       }
-       data <- x$get()
-       m <- mean(data, ...)
-       x$setmean(m)
-       m
+
+## Write a short comment describing this function
+##This is used to get the cache data
+
+cacheSolve <- function(x, ...) ##gets cache data
+  {
+  inv<-x$getinv()
+  if(!is.null(inv)){                 #checking whether inverse is null
+                    message("getting cached data!")
+                    return(inv)                        #returns inverse value
+  }
+  data<-x$get()
+  inv<-solve(data,...)              #calculates inverse value
+  x$setinv(inv)
+  inv   ##Return a matrix that is the inverse of 'x'
 }
